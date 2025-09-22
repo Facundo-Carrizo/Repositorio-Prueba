bbc3bdb495b4212439e023f8d9c69edcc4f3293d
Facundo-Carrizo <facucarrizo023@gmai.com>
2025-09-22 10:58:39 -0300

    se agrego una carpeta de prueba

--- AppPrueba/obj/Debug/net8.0/AppPrueba.AssemblyInfo.cs
+++ AppPrueba/obj/Debug/net8.0/AppPrueba.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppPrueba")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4338487d36cfdaf07e334fea8886c98cfea4413d")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppPrueba")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppPrueba")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
